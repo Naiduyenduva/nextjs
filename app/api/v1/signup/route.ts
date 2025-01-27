1c7e670026f85c763d61a2c00089dfa87e4f13c3
Laxmunaidu <luckyyenduva8.7@gmail.com>
2025-01-28 00:07:17 +0530

    added prisma postgres

--- app/api/v1/signup/route.ts
+++ app/api/v1/signup/route.ts
@@ -1,7 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const prismaClient = new PrismaClient();
 
 export async function POST (req:NextRequest) {
     const data = await req.json();
+
+    await prismaClient.user.create({
+        data: {
+            username: data.username,
+            password: data.password
+        }
+    })
+
     console.log(data);
     return NextResponse.json({
         message: "you signed up"
--- app/user/signin/page.tsx
+++ app/user/signin/page.tsx
@@ -1,11 +1,10 @@
 "use client"
 import axios from "axios"
 import { useState } from "react"
-import { useRouter } from "next/navigation"
-export default function Signup () {
+
+export default function Signin () {
     const [ username, setUsername ] = useState("")
     const [ password, setPassword ] = useState("")
-    const router = useRouter();
 
     async function handleSignup () {
         console.log(username)
@@ -14,14 +13,13 @@ export default function Signup () {
             username,
             password
         })
-        router.push("/user/signin")
     }
 
     return (
         <div className="text-black grid gap-5 w-72 ml-96 mt-60 bg-blue-600 p-5">
-            <input placeholder="username" className="h-8 rounded-lg pl-2" onChange={(e)=>{setUsername(e.target.value)}} />
-            <input placeholder="password" className="h-8 rounded-lg pl-2" onChange={(e)=>{setPassword(e.target.value)}} />
-            <button onClick={handleSignup} className="bg-white rounded-lg h-8">Signup</button>
+            <input placeholder="usernamesign" className="h-8 rounded-lg pl-2" onChange={(e)=>{setUsername(e.target.value)}} />
+            <input placeholder="passwordsign" className="h-8 rounded-lg pl-2" onChange={(e)=>{setPassword(e.target.value)}} />
+            <button onClick={handleSignup} className="bg-white rounded-lg h-8">signin</button>
         </div>
     )
 }
